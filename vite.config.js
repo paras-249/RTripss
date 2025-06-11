2e5c3a60c26dc0fddfe98d376a7916b60ea8d5f9
paras <doyeparas196@gmail.com>
2025-06-11 22:30:05 +0530

    n

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/RTrpss/', // Important! This must match your repository name
+  base: '/', // Important! This must match your repository name
 })
