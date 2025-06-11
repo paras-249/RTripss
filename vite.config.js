77673c43295af63b6a7533ea009fa932bbceadcb
paras <doyeparas196@gmail.com>
2025-06-11 10:41:55 +0530

    n

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/RTripss/', // <-- your repo name, with slashes
+  base: '/', // <-- your repo name, with slashes
   plugins: [react()],
 })
